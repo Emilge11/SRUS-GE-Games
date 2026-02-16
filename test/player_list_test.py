1a3c8a3bcdabb9cd37de206edd4c3c0beeb3e1f7
Emilge11 <J389061@tafe.wa.edu.au>
2026-02-16 14:22:58 +0800

    Added player list class tests

---
+++ test/player_list_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.player_list import PlayerList
+from app.player_node import PlayerNode
+from app.player import Player
+
+class TestPlayerList(unittest.TestCase):
+    def test_is_empty(self):
+        test_list = PlayerList()
+        result = test_list.is_empty()
+        self.assertEqual(result, True)
+
+    def test_insert_first(self):
+        test_list = PlayerList()
+        test_node = PlayerNode((Player(1,'Ibel')))
+        test_list.insert_first(test_node)
+        result = test_list.is_empty()
+        self.assertEqual(result, False)
+
+if __name__ == '__main__':
+    unittest.main()
