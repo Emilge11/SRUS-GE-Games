2eb8cf6f18f49e2f79970389a5dbc29f193bd1a7
Emilge11 <J389061@tafe.wa.edu.au>
2026-02-16 13:31:36 +0800

    Added node import, insert_first and is_empty methods

--- app/player_list.py
+++ app/player_list.py
@@ -1,4 +1,15 @@
+from app.player_node import PlayerNode
+
 class PlayerList:
     def __init__(self):
         self._head = None
+
+    def insert_first():
+        new_node = PlayerNode()
+        if self.is_empty():
+            self.head = new_node
+    
+    def is_empty(self):
+        if self.head is None:
+            return True
         
