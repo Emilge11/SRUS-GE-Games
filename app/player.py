6b72858f56424a5947fc639c146c29bd9afabd9e
Geofrey Emil <J389061@tafe.wa.edu.au>
2026-02-28 15:28:27 +0800

    Fix naming of id variable to "uid"

--- app/player.py
+++ app/player.py
@@ -1,13 +1,13 @@
 class Player:
     def __init__(self, id, name):
-        self._id = id
+        self._uid = id
         self._name = name
 
     def uid(self):
-        return self._id
+        return self._uid
     
     def name(self):
         return self._name
     
     def __str__(self):
-        return f'Name: {self._name}, ID: {self._id}'
+        return f'Name: {self._name}, ID: {self._uid}'
