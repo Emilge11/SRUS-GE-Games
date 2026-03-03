ef6ccb657d701fa3e40ebcac9f56b512c69acdc8
Geofrey Emil (306) <J389061@tafe.wa.edu.au>
2026-03-03 10:58:11 +0800

    Added tests for delete from head method

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -20,6 +20,7 @@ class TestPlayerList(unittest.TestCase):
         result1 = test_head._current.key
         result2 = test_head._previous
         result3 = test_head._next
+
         self.assertEqual(result1, 1)
         self.assertIsNone(result2, None)
         self.assertIsNone(result3, None)
@@ -117,6 +118,48 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(result1, 3)
         self.assertEqual(result2, 2)
         self.assertIsNone(result3, None)
+    
+    def test_deleted_head(self):
+        test_list = PlayerList()
+
+        test_node1 = PlayerNode(Player(1, "Bob"))
+        test_node2 = PlayerNode(Player(2, "Alice"))
+
+        test_list.insert_first(test_node1)
+        test_list.insert_first(test_node2)
+
+        test_list.delete_head()
+
+        test_head = test_list.head
+        result1 = test_head._current.key
+        result2 = test_head._previous
+        self.assertEqual(result1, 1)
+        self.assertIsNone(result2, None)
+    
+    def test_deleted_head_multi(self):
+        test_list = PlayerList()
+
+        test_node1 = PlayerNode(Player(1, "Bob"))
+        test_node2 = PlayerNode(Player(2, "Alice"))
+        test_node3 = PlayerNode(Player(3, "John"))
+        test_node4 = PlayerNode(Player(4, "Mark"))
+
+        test_list.insert_first(test_node1)
+        test_list.insert_first(test_node2)
+        test_list.insert_first(test_node3)
+        test_list.insert_first(test_node4)
+
+        test_list.delete_head()
+        test_list.delete_head()
+
+        test_head = test_list.head
+        result1 = test_head._current.key
+        result2 = test_head._previous
+        result3 = test_head._next._current.key
+        
+        self.assertEqual(result1, 2)
+        self.assertIsNone(result2, None)
+        self.assertEqual(result3, 1)
 
 if __name__ == '__main__':
     unittest.main()
