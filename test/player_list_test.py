93f20fc15d0f9f68459c876583c2725c99c3f898
Geofrey Emil (306) <J389061@tafe.wa.edu.au>
2026-03-03 10:21:57 +0800

    Added tests for inserting at list tail

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -58,7 +58,62 @@ class TestPlayerList(unittest.TestCase):
         result1 = test_head._current.key
         result2 = test_head._next._current.key
         result3 = test_head._previous
-        
+
+        self.assertEqual(result1, 3)
+        self.assertEqual(result2, 2)
+        self.assertIsNone(result3, None)
+    
+    def test_insert_tail_into_empty_list(self):
+        test_list = PlayerList()
+
+        test_node = PlayerNode(Player(1, 'Bob'))
+        test_list.insert_tail(test_node)
+
+        test_tail = test_list.tail
+        test_head = test_list.head
+        result1 = test_tail._current.key
+        result2 = test_head._current.key
+
+        self.assertEqual(result1, 1)
+        self.assertEqual(result2, 1)
+    
+    def test_insert_tail_into_single_list(self):
+        test_list = PlayerList()
+
+        test_node1 = PlayerNode(Player(1, 'Bob'))
+        test_list.insert_first(test_node1)
+
+        test_node2 = PlayerNode(Player(2, 'Alice'))
+        test_list.insert_tail(test_node2)
+
+        test_tail = test_list.tail
+        tail_prev = test_tail._previous
+        result1 = test_tail._current.key
+        result2 = tail_prev._current.key
+        result3 = test_tail.next
+
+        self.assertEqual(result1, 2)
+        self.assertEqual(result2, 1)
+        self.assertIsNone(result3, None)
+    
+    def test_insert_tail_into_multi_list(self):
+        test_list = PlayerList()
+
+        test_node1 = PlayerNode(Player(1, 'Bob'))
+        test_list.insert_first(test_node1)
+
+        test_node2 = PlayerNode(Player(2, 'Alice'))
+        test_list.insert_tail(test_node2)
+
+        test_node3 = PlayerNode(Player(3, 'Charlie'))
+        test_list.insert_tail(test_node3)
+
+        test_tail = test_list.tail
+        tail_prev = test_tail.previous
+        result1 = test_tail._current.key
+        result2 = tail_prev._current.key
+        result3 = test_tail.next
+
         self.assertEqual(result1, 3)
         self.assertEqual(result2, 2)
         self.assertIsNone(result3, None)
