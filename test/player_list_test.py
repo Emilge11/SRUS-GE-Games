8649d080ee31fa81d3d1498964b131e2dceb6c50
Geofrey Emil <J389061@tafe.wa.edu.au>
2026-03-08 16:15:27 +0800

    Added: Test cases for delete by key

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -115,5 +115,40 @@ class TestPlayerList(unittest.TestCase):
         # Assert after 4th delete - list is empty
         self.assertEqual(test_list.delete_tail(), "The list is empty.")
 
+    def test_delete_key_empty_list(self):
+        test_list = PlayerList()
+
+        # Assert  - empty list
+        self.assertEqual(test_list.delete_by_key(1), "The list is empty.")
+
+    def test_delete_key(self):
+        test_list = PlayerList()
+
+        test_node1 = PlayerNode(Player(1, "Bob"))
+        test_list.insert_first(test_node1)
+
+        test_node2 = PlayerNode(Player(2, "Alice"))
+        test_list.insert_first(test_node2)
+
+        test_node3 = PlayerNode(Player(3, "John"))
+        test_list.insert_tail(test_node3)
+
+        test_list.delete_by_key(3)
+
+        # Assert after 1st delete by key
+        self.assertEqual(test_list.tail._current.key, 1)
+        self.assertIsNone(test_list.tail._next)
+        self.assertEqual(test_list.tail._previous._current.key, 2)
+
+        test_list.delete_by_key(1)
+
+        # Assert after 2nd delete by key
+        self.assertEqual(test_list.head._current.key, 2)
+        self.assertIsNone(test_list.head._next)
+        self.assertIsNone(test_list.head._previous)
+
+        # Assert after 3rd delete by key - not found
+        self.assertEqual(test_list.delete_by_key(3), "Key not found")
+
 if __name__ == '__main__':
     unittest.main()
